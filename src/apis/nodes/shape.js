4486a08f50a6b533a4246f8043973a5eed5f17a0
Matthew Bunday <zenceph@gmail.com>
2016-12-16 01:06:10 -0500

    Fix proptype usage

--- src/apis/nodes/shape.js
+++ src/apis/nodes/shape.js
@@ -1,8 +1,8 @@
 import { PropTypes } from 'react'
 
 const shape = PropTypes.shape({
-  content: PropTypes.type.string.isRequired,
-  node: PropTypes.type.number.isRequired,
+  content: PropTypes.string.isRequired,
+  node: PropTypes.number.isRequired,
 })
 
 export default shape
