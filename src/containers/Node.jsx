0a1aef7bcb9cfa7f262f10905b0c3ae5f5db377f
Matthew Bunday <zenceph@gmail.com>
2016-12-19 23:11:12 -0600

    Add a NodeContainer

---
+++ src/containers/Node.jsx
@@ -0,0 +1,73 @@
+import React, { Component, PropTypes } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import * as NodeActions from '~/apis/nodes/actions'
+import nodeShape from '~/apis/nodes/shape'
+
+class NodeContainer extends Component {
+  componentWillMount() {
+    const { actions: { GET }, id } = this.props
+    GET(id)
+  }
+
+  render() {
+    const { confirmed, requested, failed, node, children } = this.props
+    return (
+      <div>
+        {React.Children.map(children, child =>
+          React.cloneElement(child, {
+            node,
+            confirmed,
+            requested,
+            failed,
+          }))}
+      </div>
+    )
+  }
+}
+
+NodeContainer.propTypes = {
+  id: PropTypes.string.isRequired, // param
+  confirmed: PropTypes.bool.isRequired,
+  requested: PropTypes.bool.isRequired,
+  failed: PropTypes.bool.isRequired,
+  node: nodeShape,
+  actions: PropTypes.objectOf(PropTypes.func).isRequired,
+}
+
+function mapStateToProps(state) {
+  const {
+    data: node,
+    GET: {
+      confirmed,
+      requested,
+      failed,
+    },
+  } = state.nodes.http.things[1] || {
+    data: {},
+    GET: {
+      confirmed: false,
+      requested: true,
+      failed: false,
+    },
+  }
+
+  return {
+    counter: state.counter,
+    node,
+    confirmed,
+    requested,
+    failed,
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(NodeActions, dispatch),
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(NodeContainer)
