5e5ce4e9636bc9d131e990847f9a390df7933862
Matthew Bunday <zenceph@gmail.com>
2016-12-24 01:46:37 -0600

    Remove cruft from Portal

--- src/components/Portal.jsx
+++ src/components/Portal.jsx
@@ -6,19 +6,9 @@ import ShowConfirmed from '~/components/ShowConfirmed'
 import ZEditor from '~/components/ZEditor'
 
 export default class Portal extends Component {
-  state = {
-    clicked: 0,
-  }
-
-  incClick = () => {
-    const { clicked } = this.state
-    this.setState({ clicked: clicked + 1 })
-  }
-
   render() {
-    const { clicked } = this.state
     return (
-      <div onClick={this.incClick}>
+      <div>
         <NodeContainer id="2">
           <ShowConfirmed>
             <ZEditor />
