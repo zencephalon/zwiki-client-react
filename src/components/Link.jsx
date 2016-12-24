ade4c0d96b49f6626d06cccd6333252eeb44945f
Matthew Bunday <zenceph@gmail.com>
2016-12-24 01:42:33 -0600

    Partly fold down Link component, needs to expand later

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,35 +1,11 @@
 import React, { Component, PropTypes } from 'react'
 
-import NodeContainer from '~/containers/Node'
-
-import ShowConfirmed from '~/components/ShowConfirmed'
-import ZEditor from '~/components/ZEditor'
-
-export default class Portal extends Component {
-  state = {
-    clicked: 0,
-  }
-
-  componentWillMount() {
-    this.props.insertPortal(1)
-  }
-
-  incClick = () => {
-    const { clicked } = this.state
-    this.setState({ clicked: clicked + 1 })
-  }
-
+export default class Link extends Component {
   render() {
-    const { clicked } = this.state
     console.log('inside portal render', this.props.children[0].props.text)
     return (
-      <div onClick={this.incClick}>
+      <div>
         {this.props.children}
-        {/*<NodeContainer id="2">
-          <ShowConfirmed>
-            <ZEditor />
-          </ShowConfirmed>
-        </NodeContainer>*/}
       </div>
     )
   }
