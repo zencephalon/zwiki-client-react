6dc61d02af3fd478e3da99228adf1f022df00bad
Matthew Bunday <zenceph@gmail.com>
2016-12-24 01:59:56 -0600

    Insert a portal when clicking a link

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,10 +1,16 @@
 import React, { Component, PropTypes } from 'react'
 
 export default class Link extends Component {
+  onClick = () => {
+    const { insertPortal, children } = this.props
+    // insertPortal(children[0].props.text)
+    insertPortal(2)
+  }
+
   render() {
     console.log('inside portal render', this.props.children[0].props.text)
     return (
-      <div style={{ color: '#36AECC' }}>
+      <div style={{ color: '#36AECC' }} onClick={this.onClick}>
         {this.props.children}
       </div>
     )
