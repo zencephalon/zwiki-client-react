51c0d1989c2f825181c7c19f615cd60be7a00c62
Matthew Bunday <zenceph@gmail.com>
2016-12-26 00:28:49 -0600

    Portals actually load a given node id

--- src/components/Portal.jsx
+++ src/components/Portal.jsx
@@ -5,14 +5,19 @@ import NodeContainer from '~/containers/Node'
 import ShowConfirmed from '~/components/ShowConfirmed'
 import ZEditor from '~/components/ZEditor'
 
+import { Entity } from 'draft-js'
+
 export default class Portal extends Component {
   render() {
+    const entityId = this.props.block.getEntityAt(0)
+    const entity = Entity.get(entityId)
+    const id = entity.data.id
     return (
       <div onClick={() => {
         this.props.blockProps.onClick()
         this.editor.focus()
       }}>
-        <NodeContainer id="2">
+        <NodeContainer id={id}>
           <ShowConfirmed>
             <ZEditor editorRef={(el) => { this.editor = el }} />
           </ShowConfirmed>
