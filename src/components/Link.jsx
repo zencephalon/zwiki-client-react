03ae1cdf3b87b67d712f2fda3a8cc705becd090c
Matthew Bunday <zenceph@gmail.com>
2016-12-26 00:50:49 -0600

    Links open to the specified node

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,9 +1,13 @@
 import React, { Component, PropTypes } from 'react'
 
+import { LINK_REGEX } from '~/constants'
+
 export default class Link extends Component {
   onClick = () => {
     const { insertPortal, children, moveToEnd, decoratedText } = this.props
-    moveToEnd(decoratedText, () => { insertPortal(2) })
+    moveToEnd(decoratedText, () => {
+      insertPortal(LINK_REGEX.exec(decoratedText)[2])
+    })
   }
 
   render() {
