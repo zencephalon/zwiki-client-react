a2da9d4c132c33754c4f0def5270fec26ad9da21
Matthew Bunday <zenceph@gmail.com>
2017-02-11 00:57:04 -0500

    Assure we can get to omni-search from anywhere

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { Component, PropTypes } from 'react'
+
+import { mouseTrap } from 'react-mousetrap'
+import { connect } from 'react-redux'
 
 import OmniSearch from '~/components/OmniSearch'
 
-const App = (props) => (
-  <div className="main-app-container">
-    <OmniSearch />
-    {props.children}
-  </div>
-)
+import { FOCUS } from '~/apis/flex/actions'
+import { OMNI_SEARCH } from '~/constants'
 
-export default App
+class App extends Component {
+  componentWillMount() {
+    const { dispatch, bindShortcut } = this.props
+    bindShortcut('ctrl+space', () => {
+      dispatch(FOCUS({ type: OMNI_SEARCH }))
+    })
+  }
+
+  render() {
+    return (
+      <div className="main-app-container">
+        <OmniSearch />
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+App.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  bindShortcut: PropTypes.func.isRequired,
+}
+
+export default mouseTrap(connect()(App))
