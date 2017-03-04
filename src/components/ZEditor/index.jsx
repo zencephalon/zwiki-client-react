1fd248f65953498da5200088b8e08bfe51f22cff
Matthew Bunday <zenceph@gmail.com>
2017-03-04 11:18:12 -0500

    Try a potentially simpler approach

--- src/components/ZEditor/index.jsx
+++ src/components/ZEditor/index.jsx
@@ -98,6 +98,7 @@ class ZEditor extends Component {
         focusType === EDITOR &&
         (lastProps.focusType !== EDITOR || !lastProps.focused)) {
       this.focus()
+      $.event.trigger({ type: 'keypress' })
     }
   }
 
