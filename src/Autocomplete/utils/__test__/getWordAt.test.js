83ec0407e3e8a8567e264efdfb26fb893d2a7894
Matthew Bunday <zenceph@gmail.com>
2017-10-05 18:25:18 -0400

    Got those tests going

--- src/Autocomplete/utils/__test__/getWordAt.test.js
+++ src/Autocomplete/utils/__test__/getWordAt.test.js
@@ -1,31 +1,28 @@
-import { expect } from 'chai'
 import getWordAt from '../getWordAt'
 
-describe('getWordAt', () => {
-  it('finds a word in between sentences', () => {
-    const expected = {
-      word: 'is',
-      begin: 5,
-      end: 7,
-    }
-    expect(getWordAt('this is a test', 5)).to.deep.equal(expected)
-  })
+test('finds a word in between sentences', () => {
+  const expected = {
+    word: 'is',
+    begin: 5,
+    end: 7,
+  }
+  expect(getWordAt('this is a test', 5)).toEqual(expected)
+})
 
-  it('finds the first word', () => {
-    const expected = {
-      word: 'this',
-      begin: 0,
-      end: 4,
-    }
-    expect(getWordAt('this is a test', 0)).to.deep.equal(expected)
-  })
+test('finds the first word', () => {
+  const expected = {
+    word: 'this',
+    begin: 0,
+    end: 4,
+  }
+  expect(getWordAt('this is a test', 0)).toEqual(expected)
+})
 
-  it('finds the last word', () => {
-    const expected = {
-      word: 'test',
-      begin: 10,
-      end: 14,
-    }
-    expect(getWordAt('this is a test', 15)).to.deep.equal(expected)
-  })
+test('finds the last word', () => {
+  const expected = {
+    word: 'test',
+    begin: 10,
+    end: 14,
+  }
+  expect(getWordAt('this is a test', 15)).toEqual(expected)
 })
