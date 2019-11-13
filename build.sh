5fddf8b282da6f92d43c0ae84b0b2af8b3faebeb
Matthew Bunday <mkbunday@gmail.com>
2019-11-13 10:37:15 -0500

    Update build script

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 npm run-script build
-sudo cp index.html dist/bundle.js /usr/share/nginx/html/zwiki-app/
+sudo cp index.html /usr/share/nginx/html/zwiki-app/
+sudo cp dist/bundle.js /usr/share/nginx/html/zwiki-app/static
