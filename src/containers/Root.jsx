3078612caebc3a562fcb397cb1c42b3922e700c1
Matthew Bunday <mkbunday@gmail.com>
2020-01-26 23:03:52 -0500

    Remove cruft

--- src/containers/Root.jsx
+++ src/containers/Root.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { syncHistoryWithStore } from "react-router-redux";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { configureStore } from "~/store/configureStore";
 import App from "./App";
-import Flex from "./Flex";
 
 const store = configureStore();
 
