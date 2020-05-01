ef493b584a052fd944d9374e061ea9c76bd8e1c0
Matthew Bunday <zenceph@gmail.com>
2020-05-01 12:39:11 -0400

    Webpack build works

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -11,12 +11,6 @@ module.exports = {
   },
   plugins: [
     /**
-     * This plugin assigns the module and chunk ids by occurence count. What this
-     * means is that frequently used IDs will get lower/shorter IDs - so they become
-     * more predictable.
-     */
-    new webpack.optimize.OccurenceOrderPlugin(),
-    /**
      * See description in 'webpack.config.dev' for more info.
      */
     new webpack.DefinePlugin({
@@ -24,16 +18,6 @@ module.exports = {
     }),
     new webpack.DefinePlugin({
       ZWIKI_ENV: JSON.stringify(process.env.ZWIKI_ENV || "prod")
-    }),
-    /**
-     * Some of you might recognize this! It minimizes all your JS output of chunks.
-     * Loaders are switched into a minmizing mode. Obviously, you'd only want to run
-     * your production code through this!
-     */
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        warnings: false
-      }
     })
   ],
   module: {
