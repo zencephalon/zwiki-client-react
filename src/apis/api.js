27cff0603e154098c20a262d9215b5b2cbc5cca1
Matthew Bunday <mkbunday@gmail.com>
2021-09-20 15:39:56 -0400

    Point to Digital Ocean

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,18 +1,18 @@
-import { configureAPI } from 'redux-rest-reducer'
+import { configureAPI } from 'redux-rest-reducer';
 
 const apiMap = {
   stage: 'http://api.iluvu.ninja/',
   dev: 'http://localhost:8000/',
-  prod: 'https://zw-api.zencephalon.com/',
-}
+  prod: 'https://zwiki-api-rails-2jaxy.ondigitalocean.app/',
+};
 
-export const API_BASE = apiMap[ZWIKI_ENV]
+export const API_BASE = apiMap[ZWIKI_ENV];
 
 const setAuthHeader = (headers) => {
-  const token = localStorage.getItem('token')
-  return { ...headers, Authorization: `${token}` }
-}
+  const token = localStorage.getItem('token');
+  return { ...headers, Authorization: `${token}` };
+};
 
-const api = configureAPI(API_BASE, { headerFunc: setAuthHeader })
+const api = configureAPI(API_BASE, { headerFunc: setAuthHeader });
 
-export default api
+export default api;
