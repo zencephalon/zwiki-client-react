6e096f84af26047515770749becd11fa35da306d
Matthew Bunday <mkbunday@gmail.com>
2022-11-01 11:50:33 -0400

    Refactor Link component to work with imports

--- src/constants.js
+++ src/constants.js
@@ -5,10 +5,30 @@ export const ROOT = 'root';
 export const zeroWidthNonJoiner = '‌';
 
 // From Slimdown
-export const LINK_REGEX = /\[([^[]+)\]\(([^)]+)\)/g;
-export const IMPORT_REGEX = /\{([^{]+)\}\(([^)]+)\)/g;
-export const LINK_AND_IMPORT_REGEX = /(\[([^[]+)\]|\{([^{]+)\})\(([^)]+)\)/g;
-export const LINK_REGEX_NO_G = /\[([^[]+)\]\(([^)]+)\)/;
+// export const LINK_REGEX_NO_G = /\[([^[]+)\]\(([^)]+)\)/;
+// export const LINK_AND_IMPORT_REGEX = /(\[([^[]+)\]|\{([^{]+)\})\(([^)]+)\)/g;
+// export const LINK_AND_IMPORT_REGEX_NO_G = /(\[([^[]+)\]|\{([^{]+)\})\(([^)]+)\)/;
+
+const leftBraceBracket = String.raw`(?:\{|\[)`;
+const rightBraceBracket = String.raw`(?:\}|\])`;
+const innerTextCapture = String.raw`([^[{]+)`;
+
+const braceCapture = String.raw`\{([^{]+)\}`;
+const bracketCapture = String.raw`\[([^[]+)\]`;
+const parenCapture = String.raw`\(([^)]+)\)`;
+export const LINK_REGEX_NO_G = RegExp(`${bracketCapture}${parenCapture}`);
+export const LINK_REGEX = RegExp(`${bracketCapture}${parenCapture}`, 'g');
+
+export const IMPORT_REGEX_NO_G = RegExp(`${braceCapture}${parenCapture}`);
+export const IMPORT_REGEX = RegExp(`${braceCapture}${parenCapture}`, 'g');
+
+export const LINK_AND_IMPORT_REGEX = RegExp(
+  `${leftBraceBracket}${innerTextCapture}${rightBraceBracket}${parenCapture}`,
+  'g'
+);
+export const LINK_AND_IMPORT_REGEX_NO_G = RegExp(
+  `${leftBraceBracket}${innerTextCapture}${rightBraceBracket}${parenCapture}`
+);
 
 export const NOT_DONE_TODO = '☐';
 export const DONE_TODO = '☑';
---
+++ src/constants.test.js
@@ -0,0 +1,29 @@
+import {
+  LINK_REGEX_NO_G,
+  IMPORT_REGEX_NO_G,
+  LINK_AND_IMPORT_REGEX_NO_G,
+} from './constants';
+
+test('LINK_REGEX_NO_G', () => {
+  const match = LINK_REGEX_NO_G.exec('foo [bar](baz) qux');
+
+  expect(match[0]).toEqual('[bar](baz)');
+  expect(match[1]).toEqual('bar');
+  expect(match[2]).toEqual('baz');
+});
+
+test('IMPORT_REGEX_NO_G', () => {
+  const match = IMPORT_REGEX_NO_G.exec('foo {bar}(baz) qux');
+
+  expect(match[0]).toEqual('{bar}(baz)');
+  expect(match[1]).toEqual('bar');
+  expect(match[2]).toEqual('baz');
+});
+
+test('LINK_AND_IMPORT_REGEX_NO_G', () => {
+  const match = LINK_AND_IMPORT_REGEX_NO_G.exec('foo {bar}(baz) qux');
+
+  expect(match[0]).toEqual('{bar}(baz)');
+  expect(match[1]).toEqual('bar');
+  expect(match[2]).toEqual('baz');
+});
