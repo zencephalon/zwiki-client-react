374cd05cd0aaaaf08a9484a728e347b0b3bfd3db
Matthew Bunday <mkbunday@gmail.com>
2022-11-26 23:29:53 -0500

    Move prettier to dev deps

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,44 +1,44 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  devtool: "cheap-module-source-map",
-  entry: ["./src/index.jsx"],
+  devtool: 'cheap-module-source-map',
+  entry: ['./src/index.jsx'],
   output: {
-    path: path.join(__dirname, "dist"),
-    filename: "bundle.js",
-    publicPath: "/static/"
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/',
   },
   plugins: [
     /**
      * See description in 'webpack.config.dev' for more info.
      */
     new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify("production")
+      'process.env.NODE_ENV': JSON.stringify('production'),
     }),
     new webpack.DefinePlugin({
-      ZWIKI_ENV: JSON.stringify(process.env.ZWIKI_ENV || "prod")
-    })
+      ZWIKI_ENV: JSON.stringify(process.env.ZWIKI_ENV || 'prod'),
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        loaders: ["babel-loader"],
-        include: path.join(__dirname, "src")
+        loaders: ['babel-loader'],
+        include: path.join(__dirname, 'src'),
       },
       {
         test: /\.scss$/,
-        loader: "style-loader!css-loader!sass-loader"
+        loader: 'style-loader!css-loader!sass-loader',
       },
       {
         test: /\.css$/,
         exclude: /\.useable\.css$/,
-        loader: "style-loader!css-loader"
-      }
-    ]
+        loader: 'style-loader!css-loader',
+      },
+    ],
   },
   resolve: {
-    extensions: [".js", ".jsx"]
-  }
+    extensions: ['.js', '.jsx'],
+  },
 };
